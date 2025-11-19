73860935983cd047c87f0844054a4474577e3166
MatteoBraida-ITS <matteo.braida@allievi.itsdigitalacademy.com>
2025-11-20 00:08:22 +0100

    Inizio progetto

---
+++ Sistema_alunni_ITS.py
@@ -0,0 +1,15 @@
+import json
+import time
+from datetime import datetime
+import os
+
+lista_alunni = {
+    "MAT001":{
+        "nome": "Matteo",
+        "cognome": "Braida",
+        "email": "matteo.braida@its.com",
+        "matricola": "MAT001",
+        "data_creazione":,
+        "data_modifica":
+    }
+}
