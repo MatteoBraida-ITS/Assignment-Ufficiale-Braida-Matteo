9f38067f5645af76cda4354bcdd41569c3cd27fa
MatteoBraida-ITS <matteo.braida@allievi.itsdigitalacademy.com>
2025-11-20 16:16:34 +0100

    modifica 20/11/25

--- Sistema_alunni_ITS.py
+++ Sistema_alunni_ITS.py
@@ -3,13 +3,31 @@ import time
 from datetime import datetime
 import os
 
-lista_alunni = {
-    "MAT001":{
-        "nome": "Matteo",
-        "cognome": "Braida",
-        "email": "matteo.braida@its.com",
-        "matricola": "MAT001",
-        "data_creazione":,
-        "data_modifica":
-    }
-}
+while True:
+
+    print("=" * 50)
+    
+
+
+#timestamp = datetime.now()
+#
+#lista_alunni = {
+#    "MAT001":{
+#        "nome": "Matteo",
+#        "cognome": "Braida",
+#        "email": "matteo.braida@its.com",
+#        "matricola": "MAT001",
+#        "data_creazione":f"{timestamp}",
+#        "data_modifica":f"{timestamp}"
+#    }
+#}
+#
+#def creo_json():
+#    with open("lista_alunni.json", "w") as file:
+#        json.dump(lista_alunni, file, indent=4)
+#
+#creo_json()
+#
+#
+#
+#print(lista_alunni)
