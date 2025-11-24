da39228d5ccc274274533f8d91a75f451204b5b4
MatteoBraida-ITS <matteo.braida@allievi.itsdigitalacademy.com>
2025-11-24 18:00:04 +0100

    Funzione per aggiunta alunno e creazione matricola.

--- Sistema_alunni_ITS.py
+++ Sistema_alunni_ITS.py
@@ -4,31 +4,42 @@ from datetime import datetime
 import os
 
 timestamp = datetime.now()
-generazione_matricola = f"MAT{}"
+
+def crea_matricola():
+    id_matricola = "000"
+    id_matricola = '%03d' % (int(id_matricola) + 1)
+    return id_matricola
+
+crea_matricola()
 
 lista_alunni = {
-    f"{generazione_matricola}":{
+    f"MAT{crea_matricola()}":{
         "nome": "Matteo",
         "cognome": "Braida",
         "email": "matteo.braida@its.com",
-        "matricola": "MAT001",
+        "matricola": f"MAT{crea_matricola()}",
         "data_creazione":f"{timestamp}",
         "data_modifica":f"{timestamp}"
     },
 }
 
-nuovo_alunno = {}
-
-def creo_json():
+def crea_json():
     with open("lista_alunni.json", "w") as file:
         json.dump(lista_alunni, file, indent=4)
 
-creo_json()
+crea_json()
 
 def aggiunta_alunno():
-    matricola = input("Insersci ")
-
-
+    nuovo_alunno = {}
+    nome_alunno = input("Insersci nome alunno:")
+    nuovo_alunno.update({"nome": nome_alunno})
+    cognome_alunno = input("Inserisci cognome:")
+    nuovo_alunno.update({"cognome": cognome_alunno})
+    email_alunno = input("Inserisci e-mail:")
+    nuovo_alunno.update({"email": email_alunno})
+    nuovo_alunno.update({"data_creazione":f"{timestamp}"})
+    nuovo_alunno.update({"data_modifica":f"{timestamp}"})
+    
 
 print(lista_alunni)
 
@@ -37,19 +48,22 @@ while True:
     print("\nSISTEMA TRACCIAMENTO ALUNNI")
     print("===========================")
     print("\nSeleziona un'opzione:")
-    print("     1)Inserisci nuvo alunno")
-    print("     2)Visualizza alunni registrati")
-    print("     3)Modifica dati alunno")
-    print("     4)Elimina alunno")
-    print("     5)Assegna compito a studente")
-    print("     6)Registra valutazione")
-    print("     7)Visualizza compiti di uno studente")
-    print("     8)Visualizza statistiche alunno")
-    print("     9)Ranking alunni per media voti")
-    print("     10)Report compiti non completati")
-    print("     11)Salva dati (backup)")
-    print("     12)Carica dati")
-    print("     13)Visualizza menù")
-    print("     14)Esci")
+    print("     A)Inserisci nuovo alunno")
+    print("     B)Visualizza alunni registrati")
+    print("     C)Modifica dati alunno")
+    print("     D)Elimina alunno")
+    print("     E)Assegna compito a studente")
+    print("     F)Registra valutazione")
+    print("     G)Visualizza compiti di uno studente")
+    print("     H)Visualizza statistiche alunno")
+    print("     I)Ranking alunni per media voti")
+    print("     L)Report compiti non completati")
+    print("     M)Salva dati (backup)")
+    print("     N)Carica dati")
+    print("     O)Visualizza menù")
+    print("     P)Esci")
     
-    scelta_menu = input("\nSeleziona l'opzione:")
+    scelta_menu = input("\nSeleziona l'opzione:").lower()
+
+    if scelta_menu == 'a':
+        aggiunta_alunno()
